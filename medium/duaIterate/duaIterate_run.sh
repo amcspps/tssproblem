56cc07c3ca352fb3673ba6b2feb185f5d1ebb0e6
amcspps <pashafizmat1@gmail.com>
2024-01-04 17:08:19 +0300

    medium map routes + run script

---
+++ medium/duaIterate/duaIterate_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ -d "iterations" ]; then
+    cd iterations
+else
+    mkdir "iterations"
+    cd iterations
+fi
+
+
+python /usr/share/sumo/tools/assign/duaIterate.py -n /home/pavel/dev/diplom/tssproblem/medium/net/osm.net.xml -t /home/pavel/dev/diplom/tssproblem/medium/od2trips/trips.odtrips.xml
---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <folder_name>"
+    exit 1
+fi
+
+folder_name="$1"
+
+if [ ! -d "$folder_name" ]; then
+    echo "Error: Folder '$folder_name' not found."
+    exit 1
+fi
+
+pwd
+
+cd "$folder_name/od2trips" || exit 1
+
+./od2trips_run.sh
+
+cd ".."
+
+cd "duaIterate" || exit 1
+
+./duaIterate_run.sh
+
+cd -
