286ccbdf05c301bb118a00f88c655abf85a1beb4
amcspps <pashafizmat1@gmail.com>
2024-02-21 00:35:50 +0300

    sudo check addition

--- memory.py
+++ memory.py
@@ -6,7 +6,7 @@ import argparse
 target_mount_point = '/mnt/tss-inter-logs'
 
 def is_mounted(target):
-    output = subprocess.run(['sudo', 'mount'], capture_output=True, text=True)
+    output = subprocess.run(['mount'], capture_output=True, text=True)
     
     for line in output.stdout.splitlines():
         if target in line:
@@ -18,12 +18,12 @@ def directory_exists(directory):
     return os.path.exists(directory)
 
 def create_hierarchy(target_directory):
-    subprocess.run(['sudo', 'mkdir', target_directory])
+    subprocess.run(['mkdir', target_directory])
     for name in utils.names_dict:
-        subprocess.run(['sudo','mkdir', os.path.join(target_directory, name)])
+        subprocess.run(['mkdir', os.path.join(target_directory, name)])
 
 def mount_tmpfs(target_directory):
-    subprocess.run(['sudo', 'mount', '-t', 'tmpfs', '-o', 'size=10M', 'tmpfs', target_directory])
+    subprocess.run(['mount', '-t', 'tmpfs', '-o', 'size=10M', 'tmpfs', target_directory])
      
 def mount():
     if not directory_exists(target_mount_point):
@@ -40,20 +40,25 @@ def mount():
             print(f"{target_mount_point} has been mounted.")
 
 def unmount():
-    result = subprocess.run(['sudo', 'umount','-f', target_mount_point])
-    subprocess.run(['sudo', 'rm', '-rf',  target_mount_point])
+    result = subprocess.run(['umount','-f', target_mount_point])
+    subprocess.run(['rm', '-rf',  target_mount_point])
     print(f"{target_mount_point} has been successfully unmounted. folders deleted.")
 
 parser = argparse.ArgumentParser(description='Manage tmpfs memory mounts')
+parser.add_argument('--sudo', action='store_true', help='Check if the script was started with sudo')
 parser.add_argument('-m', action='store_true', help='Mount tmpfs memory')
 parser.add_argument('-u', action='store_true', help='Unmount tmpfs memory and delete directory')
 args = parser.parse_args()
 
+def is_started_with_sudo():
+    return 'SUDO_USER' in os.environ
 
-if args.m:
-    mount()
-
-elif args.u:
-    unmount()
+if args.sudo or is_started_with_sudo():        
+    if args.m:
+        mount()
+    elif args.u:
+        unmount()
+    else:
+        print("No action specified. Use -m to mount or -u to unmount.")
 else:
-    print("No action specified. Use -m to mount or -u to unmount.")
+    print("usage: sudo python script.py")
